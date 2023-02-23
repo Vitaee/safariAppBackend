e2ca9bc9b7bf41a37e80120a54fdabde0425ed73
Vitaee <canilguu@gmail.com>
2023-02-23 20:44:53 +0200

    user auth is implemented.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login', TokenObtainPairView.as_view(), name='login'),
+    path('register', views.UserRegisterView.as_view(), name='register'),
+    path('refresh/token/', TokenRefreshView.as_view(), name="token_refresh" ),
+    path('profile', views.UserView.as_view(),name='profile_view'),
+
 ]
--- safariBackend/settings/dev.py
+++ safariBackend/settings/dev.py
@@ -1,26 +1,15 @@
 from .base import *
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-
-DOTENV_FILE=f'{BASE_DIR}/.env.development'
-config = Config(RepositoryEnv(DOTENV_FILE))
-
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config.get('DB_NAME'),
-        'USER': config.get('DB_USER'),
-        'PASSWORD': config.get('DB_PASSWORD'),
-        'HOST': config.get('DB_HOST'),
-        'PORT': config.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
