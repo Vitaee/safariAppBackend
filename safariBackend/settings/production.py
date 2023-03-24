19ff81ab7b901039b8952ffeb3d88b719655f269
Can İlgu <canilguu@gmail.com>
2023-03-24 18:45:55 +0300

    collec static error bug fix on production.

--- safariBackend/settings/production.py
+++ safariBackend/settings/production.py
@@ -16,8 +16,9 @@ DATABASES = {
         'PORT': config.get('DB_PORT'),
     }
 }
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
