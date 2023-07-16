142eb70ecf94a15c0fa56d2591f73ea64e1a1256
canilgu <canilguu@gmail.com>
2023-07-16 11:29:45 +0300

    test wroted for user fav and ratings

---
+++ accounts/tests/test_user_fav.py
@@ -0,0 +1,128 @@
+import json
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from accounts.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class TestUserView(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.username = 'testuser'
+        cls.password = 'testpassword'
+        cls.user = User.objects.create_user(username='testuser', password='testpassword')
+        refresh = RefreshToken.for_user(cls.user)
+        cls.token = str(refresh.access_token)
+
+        cls.safari_url = '/api/safari/create'
+        data = {
+        "imageCover": "https://cloudfront.safaribookings.com/lib/tanzania/tour/1184x259/Serengeti_National_Park_048.jpg",
+        "name": "7-Day Northern Parks Budget Safari Tent (Migration)",
+        "url": "https://www.safaribookings.com/tours/t9480",
+        "price": "2330.00",
+        "max_price": "0.00",
+        "ratingsQuantity": 272,
+        "ratingsAverage": "4.9",
+        "tour_data": {
+            "overview": {
+            "route_data": [
+                {
+                "day_0": {
+                    "days": "Start",
+                    "days_route": "Arusha (Day 1)"
+                },
+                "day_1": {
+                    "days": "Day 1",
+                    "days_route": "Tarangire NP"
+                },
+                "day_2": {
+                    "days": "Day 2",
+                    "days_route": "Ngorongoro Crater"
+                }
+                }
+            ],
+            "tour_features": [
+                {
+                "feature_0": {
+                    "title": "Mid-range tour",
+                    "description": "This mid-range tour uses tented camps."
+                },
+                "feature_1": {
+                    "title": "Private tour",
+                    "description": "This tour will be organized exclusively for you and won't be shared with others."
+                }
+                }
+            ],
+            "route_description": "Enjoy the best of the Northern parks in 7 days’ time! You'll be visiting Tarangire, the Ngorongoro Crater, Serengeti National Park, Lake Natron and Lake Manyara. Enjoy the beautiful landscape and great views of the animals as you do game drives around the parks. This itinerary offers a unique game drive and walking safari in the Ikona Wildlife Management Area and an overnight stay in a Maasai boma lodge! You also have a chance to see the migration in Serengeti.",
+            "accomodaton_and_meals": [
+                {
+                "accomodation_0": {
+                    "day": "7",
+                    "meals": "Breakfast & Lunch Included",
+                    "accommodation": {
+                    "image": "\n                                        End of tour(No accommodation)\n\n\n                                            – Breakfast & Lunch Included\n                                            \n",
+                    "title": "End of tour(No accommodation)                                            – Breakfast & Lunch Included",
+                    "description": "(No accommodation)"
+                    }
+                }
+                }
+            ],
+            "activities_and_transportation": [
+                "Activities: game drives &  nature hikes/walks",
+                "Game drives: pop-up roof 4x4 vehicle"
+            ]
+            }
+        },
+        "inclusions_data": {
+            "inclusions": {
+            "excluded": [
+                "International flights (From/to home)",
+                "Roundtrip airport transfer",
+            ],
+            "included": [
+                "Park fees (For non-residents)",
+                "All activities (Unless labeled as optional)",
+            ]
+            }
+        },
+        "getting_there_data": [
+            "This tour starts and ends in Arusha",
+            "This operator can help select your international flights, but you'll have to book them yourself help",
+        ],
+        "day_by_day": [
+        {
+          "day_1": {
+            "image": "https://cloudfront.safaribookings.com/lib/tanzania/destination/464x145/Tarangire_National_Park_008.jpg",
+            "description": "Today, your safari adventure begins! We pick you up from your hotel in or around Arusha. During the 2.5-hour drive, you will enjoy the view of the beautiful nature, landscapes and local communities. We’ll do a full-day game drive in Tarangire which is famous for its large numbers of elephants, baobab trees and tree-climbing African pythons. At the end of the day, we go to Africa Safari Lake Manyara. First, you can take a dive into the swimming pool!",
+            "destination": "Tarangire National Park"
+          },
+          "day_2": {
+            "image": "https://cloudfront.safaribookings.com/lib/tanzania/destination/464x145/Ngorongoro_Conservation_Area_021.jpg",
+            "description": "Today we wake up early in the morning and after breakfast, we will drive into the amazing Ngorongoro Crater. The Ngorongoro Crater is the largest intact crater in the world. The crater is home to an estimated 30,000 large mammals. Due to its natural borders, there is an abundance of wildlife throughout the conservation area. It is also home to the Big Five including the African black rhino, as well as hyena and zebra to name a few. At the end of the day we head to Ndutu/South Serengeti where we overnight at Africa Safari South Serengeti. The area is famous for the calving season of the Great Migration, an absolute highlight of the annual wildebeest migration.",
+            "destination": "Ngorongoro Crater"
+          }
+        }
+      ]
+        }
+        response = cls.client.post(cls.safari_url, data, format='json')
+        cls.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        cls.safari_id = response.data['id']
+
+    def test_post_user_favs(self):
+        url = reverse('accounts:user_favs')
+        data = {
+            "user": self.user.id,
+            "safari": self.safari_id
+
+        }
+        response = self.client.post(url, data=data ,HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['username'])
+
+    def test_get_user_favs(self):
+        url = reverse('accounts:user_favs')
+        response = self.client.get(url, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data['username'])
--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('refresh/token/', TokenRefreshView.as_view(), name="token_refresh" ),
     path('profile', views.UserView.as_view(),name='profile_view'),
     path('register', views.UserRegisterView.as_view(), name='sign_up'),
-    path('favourite', views.UserFavouritesView.as_view(), name="user_favs")
+    path('favourite', views.UserFavouritesView.as_view({'get': 'list'}), name="user_favs")
 ]
